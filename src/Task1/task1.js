6371257b631c8a1898b5e52db058b8ec292a563d
Луковцов Артем <ALukautsou@gomel.iba.by>
2022-04-26 16:56:19 +0300

    Deleted the sharp variable

--- src/Task1/task1.js
+++ src/Task1/task1.js
@@ -1,10 +1,9 @@
-function forLoop(a){
-    let str = "";
-    for (var i = 1; i <=a; i++){
-        var sharp = "#";
-        var substr = sharp.repeat(i);
+function forLoop(a) {
+    var str = "";
+    for (var i = 1; i <= a; i++) {
+        var substr = "#".repeat(i);
         substr += "/n";
         str += substr;
-    }
+        }
     return str;
 }
