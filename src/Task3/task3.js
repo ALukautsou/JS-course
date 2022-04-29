638097b7d7258873917b620e49c2c3cc0518cd45
Луковцов Артем <ALukautsou@gomel.iba.by>
2022-04-29 21:14:23 +0300

    Changed == to ===, and also fixed UpperCase

--- src/Task3/task3.js
+++ src/Task3/task3.js
@@ -1,6 +1,6 @@
-function isEven(N) {
-    if (N == 0) return Boolean(true);
-    else if (N == 1) return Boolean(false);
-    else if (N < 0) return isEven(-N);
-    else return isEven(N-2);
+function isEven(n) {
+    if (n === 0) return true;
+    else if (n === 1) return false;
+    else if (n < 0) return isEven(-n);
+    else return isEven(n-2);
 }
