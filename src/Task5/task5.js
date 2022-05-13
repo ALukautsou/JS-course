f2f423bed2398253fcc0e26c5fcd358705d4adfe
Луковцов Артем <ALukautsou@gomel.iba.by>
2022-05-14 00:30:39 +0300

    Error correction
    Changed the condition and added it to the step argument, shortened the lines and moved return to a new line.

--- src/Task5/task5.js
+++ src/Task5/task5.js
@@ -1,15 +1,11 @@
-function range(start, end, step){
-    if (step == null) step = 1;
+function range(start, end, step = start < end ? 1 : -1){
     var arr = [];
     if (step > 0){
-        for (var i = start; i <= end; i += step){
-            arr.push(i);
-        }
+        for (var i = start; i <= end; i += step) arr.push(i);
     } else {
-        for (var i = start; i >= end; i += step){
-            arr.push(i);
-        }
-    } return arr;
+        for (var i = start; i >= end; i += step) arr.push(i);
+    }
+    return arr;
 }
 
 function sum(arr){
