b0cbe4d9b3324af949c9d03a5529fbc7f0b5b19f
Луковцов Артем <ALukautsou@gomel.iba.by>
2022-05-23 22:04:34 +0300

    Added changes
    Removed if, left only one for, and changed the loop condition

--- src/Task5/task5.js
+++ src/Task5/task5.js
@@ -1,9 +1,7 @@
 function range(start, end, step = start < end ? 1 : -1){
     var arr = [];
-    if (step > 0){
-        for (var i = start; i <= end; i += step) arr.push(i);
-    } else {
-        for (var i = start; i >= end; i += step) arr.push(i);
+    for (var i = start; step < 0 ? i >= end : i <= end; i += step){
+        arr.push(i);
     }
     return arr;
 }
