df2bbfc8b645381fd153dbef3fb05308b9242b91
Луковцов Артем <ALukautsou@gomel.iba.by>
2022-07-06 01:26:58 +0300

    Fixed the condition for checking the object type and returned true after all false

--- src/Task8/task8.js
+++ src/Task8/task8.js
@@ -1,15 +1,18 @@
 function deepEqual(a, b) {
-    if (a === b) {return true;}
+  if (a === null || b === null) {return false;}
 
-    if (typeof a == "object" && a != null && typeof b == "object" && b != null) {return false;}
-    
+  if (typeof a === "object" && typeof b === "object") {
     let keysA = Object.keys(a), keysB = Object.keys(b);
 
-    if (keysA.length != keysB.length) {return false;}
-  
+    if (keysA.length !== keysB.length) {return false;}
+
     for (let key of keysA) {
-      if (!keysB.includes(key) || !deepEqual(a[key], b[key])) {return false;}
+      if (keysB.includes(key) && !deepEqual(a[key], b[key])) {return false;}
     }
-  
+
     return true;
+  } else {
+    return a === b;
   }
+}
+console.log(deepEqual(10,10));
