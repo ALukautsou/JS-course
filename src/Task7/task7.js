1d451b41058ed24391b26c7fa15f81e623142749
Луковцов Артем <ALukautsou@gomel.iba.by>
2022-07-12 20:04:10 +0300

    Added Task7

---
+++ src/Task7/task7.js
@@ -0,0 +1,29 @@
+function arrayToList(arr) {
+    var list = null;
+
+    for (var i = arr.length - 1; i >= 0; i--) {
+        list = {value: arr[i], rest: list};
+    }
+
+    return list;
+}
+
+function listToArray(list) {
+    var arr = [];
+    
+    for (var node = list; node; node = node.rest) {
+        arr.push(node.value);
+    }
+
+    return arr;
+}
+
+function prepend(key, list) {
+    var node = {value: key, rest: list};
+    return node;
+}
+
+function nth(list, num) {
+    return (!list) ? undefined :
+    (num === 0) ? list.value : nth(list.rest, num - 1);
+}
