bb77e8465b886f9220335f5f3112472c24722ce3
Луковцов Артем <ALukautsou@gomel.iba.by>
2022-07-16 18:17:20 +0300

    Added task10

---
+++ src/Task10/task10.js
@@ -0,0 +1,20 @@
+const ANCESTRY_FILE = require("../../ext/ancestry");
+
+var ancestry = JSON.parse(ANCESTRY_FILE);
+
+function average(array) {
+    function plus(a, b) { return a + b; }
+    return array.reduce(plus) / array.length;
+}
+
+var byName = {};
+
+ancestry.forEach(function(person) {
+  byName[person.name] = person;
+});
+
+var difference = ancestry.filter(function(person) {
+    return byName[person.mother] != null;
+}).map(function(person) {
+    return person.born - byName[person.mother].born;
+});
