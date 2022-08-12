f73b912f12a026c2380bd4b87c794d81086222dc
Луковцов Артем <ALukautsou@gomel.iba.by>
2022-08-12 21:14:08 +0300

    Changed the first if condition, added a type check and a value check

--- src/Task8/task8.js
+++ src/Task8/task8.js
@@ -1,6 +1,25 @@
 function deepEqual(a, b) {
-  if (a === null || b === null || !(a instanceof Object && b instanceof Object)) {
+  if (a === null || b === null) {
     return a === b;
+  }
+
+  var aLikeNumber = new Number(a);
+  var bLikeNumber = new Number(b);
+
+  if (typeof a === "boolean" || typeof b === "boolean") {
+    return a === b;
+  }
+
+  if (typeof a === "string" || typeof b === "string") {
+    return a === b;
+  }
+
+  if ((!isNaN (aLikeNumber) && aLikeNumber instanceof Number) || (!isNaN (bLikeNumber) && bLikeNumber instanceof Number)) {
+    return a === b;
+  }
+
+  if (!(a instanceof Object) || !(b instanceof Object)) {
+    return false;
   }
 
   var keysA = Object.keys(a);
@@ -11,6 +30,6 @@ function deepEqual(a, b) {
   }
 
   for (var key of keysA) {
-        return keysB.includes(key) && deepEqual(a[key], b[key])
-      }
+    return keysB.includes(key) && deepEqual(a[key], b[key]);
+  }
 }
