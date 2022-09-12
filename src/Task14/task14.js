13a5855a1c2c1b3c91d3917477e84ed47dfd669f
Луковцов Артем <ALukautsou@gomel.iba.by>
2022-09-12 17:28:28 +0300

    Fixed uppercase names to lowercaseand ++i to i++

--- src/Task14/task14.js
+++ src/Task14/task14.js
@@ -1,31 +1,31 @@
 function repeat(string, times) {
 	var result = "";
 
-	for (var i = 0; i < times; ++i) {
+	for (var i = 0; i < times; i++) {
 		result += string;
     }
     
 	return result;
 }
 
-function TextCell(text) {
+function textCell(text) {
 	this.text = text.split("\n");
 }
 
-TextCell.prototype.minWidth = function() {
+textCell.prototype.minWidth = function() {
 	return this.text.reduce(function(width, line) {
 		return Math.max(width, line.length);
 	}, 0);
 };
 
-TextCell.prototype.minHeight = function() {
+textCell.prototype.minHeight = function() {
 	return this.text.length;
 };
 
-TextCell.prototype.draw = function(width, height) {
+textCell.prototype.draw = function(width, height) {
 	var result = [];
 
-	for (var i = 0; i < height; ++i) {
+	for (var i = 0; i < height; i++) {
 		var line = this.text[i] || "";
 		result.push(line + repeat(" ", width - line.length));
 	}
@@ -33,20 +33,20 @@ TextCell.prototype.draw = function(width, height) {
 	return result;
 };
 
-function StretchCell(inner, width, height) {
+function stretchCell(inner, width, height) {
 	this.inner = inner;
 	this.width = width;
 	this.height = height;
 }
 
-StretchCell.prototype.minWidth = function() {
+stretchCell.prototype.minWidth = function() {
 	return Math.max(this.width, this.inner.minWidth());
 };
 
-StretchCell.prototype.minHeight = function() {
+stretchCell.prototype.minHeight = function() {
 	return Math.max(this.height, this.inner.minHeight());
 };
 
-StretchCell.prototype.draw = function(width, height) {
+stretchCell.prototype.draw = function(width, height) {
 	return this.inner.draw(width, height);
 }
